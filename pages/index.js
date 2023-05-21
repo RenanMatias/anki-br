fe578b9610c4e76ba6b74d4554992d2cf37e1cf0
Renan Matias <rnmatias@hotmail.com>
2023-05-21 12:40:52 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>My Pocket. Um local onde você controla suas finanças e seus investimentos.</h1>
+  return <h1>My Pocket. Um local onde você controla suas finanças e seus investimentos. 🪙</h1>
 }
 
 export default Home;
