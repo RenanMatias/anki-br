75dcfc34a548c424cbe5dd8fd50f4a054a59c608
Renan Matias <rnmatias@hotmail.com>
2023-06-29 22:10:06 +0000

    adiciona arquivos `.editorconfig` e `extensions.json`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>My Pocket. Um local onde você controla suas finanças e seus investimentos. 🪙</h1>
+  return (
+    <h1>
+      My Pocket. Um local onde você controla suas finanças e seus investimentos.
+      🪙
+    </h1>
+  );
 }
 
 export default Home;
