8c3050c63403f7b39adff3ccde3258536ae97e32
Renan Matias <rnmatias@hotmail.com>
2023-09-22 19:18:58 -0300

    add `/api/v1/status`endpoint

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,60 @@ function Home() {
 
       <h2>O que deve ser feito?</h2>
       <ul>
-          <li>Implementar uma plataforma interativa de flashcards que permita aos usuários criar e personalizar seus próprios conjuntos de cartões para estudar.</li>
-          <li>Desenvolver um sistema de repetição espaçada que ajuste automaticamente o intervalo entre as revisões, otimizando a retenção do conhecimento.</li>
-          <li>Oferecer uma experiência intuitiva e fácil de usar, para que os usuários possam navegar pelo site de forma simples e acessar rapidamente seus materiais de estudo.</li>
+        <li>
+          Implementar uma plataforma interativa de flashcards que permita aos
+          usuários criar e personalizar seus próprios conjuntos de cartões para
+          estudar.
+        </li>
+        <li>
+          Desenvolver um sistema de repetição espaçada que ajuste
+          automaticamente o intervalo entre as revisões, otimizando a retenção
+          do conhecimento.
+        </li>
+        <li>
+          Oferecer uma experiência intuitiva e fácil de usar, para que os
+          usuários possam navegar pelo site de forma simples e acessar
+          rapidamente seus materiais de estudo.
+        </li>
       </ul>
 
       <h2>O que deve ser protegido?</h2>
       <ul>
-          <li>Garantir a segurança dos dados dos usuários, implementando medidas robustas de proteção de informações pessoais e dos conjuntos de flashcards criados.</li>
-          <li>Assegurar que o processo de login e registro seja seguro e confiável, evitando possíveis violações de privacidade.</li>
+        <li>
+          Garantir a segurança dos dados dos usuários, implementando medidas
+          robustas de proteção de informações pessoais e dos conjuntos de
+          flashcards criados.
+        </li>
+        <li>
+          Assegurar que o processo de login e registro seja seguro e confiável,
+          evitando possíveis violações de privacidade.
+        </li>
       </ul>
 
       <h2>O que deve ser repetido?</h2>
       <ul>
-          <li>Destacar os benefícios da repetição espaçada como método eficaz de aprendizado, enfatizando como essa técnica ajuda a reter informações de forma duradoura.</li>
-          <li>Incentivar os usuários a criar hábitos de estudo consistentes, utilizando a repetição espaçada como ferramenta para revisar regularmente os flashcards.</li>
-          <li>Oferecer lembretes e notificações personalizadas para que os usuários nunca percam uma oportunidade de revisar e aprimorar seu conhecimento.</li>
+        <li>
+          Destacar os benefícios da repetição espaçada como método eficaz de
+          aprendizado, enfatizando como essa técnica ajuda a reter informações
+          de forma duradoura.
+        </li>
+        <li>
+          Incentivar os usuários a criar hábitos de estudo consistentes,
+          utilizando a repetição espaçada como ferramenta para revisar
+          regularmente os flashcards.
+        </li>
+        <li>
+          Oferecer lembretes e notificações personalizadas para que os usuários
+          nunca percam uma oportunidade de revisar e aprimorar seu conhecimento.
+        </li>
       </ul>
 
-      <p>Com foco nestes tópicos, o site Anki-BR poderá proporcionar uma experiência valiosa e estimulante para o ensino através da repetição espaçada, capacitando os usuários a dominar diversos assuntos de maneira sistemática e eficiente. 🚀📚</p>
+      <p>
+        Com foco nestes tópicos, o site Anki-BR poderá proporcionar uma
+        experiência valiosa e estimulante para o ensino através da repetição
+        espaçada, capacitando os usuários a dominar diversos assuntos de maneira
+        sistemática e eficiente. 🚀📚
+      </p>
     </div>
   );
 }
